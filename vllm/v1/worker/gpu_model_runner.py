e6e1904edc3264bca9b259fab2ff18cce8783c22
wwl2755-google <wenlongwang@google.com>
2025-07-28 16:19:40 -0700

    revert the dump()

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -76,7 +76,6 @@ from ..sample.logits_processor import LogitsProcessorManager
 from .utils import (MultiModalBudget, bind_kv_cache, gather_mm_placeholders,
                     initialize_kv_cache_for_kv_sharing,
                     sanity_check_mm_encoder_outputs, scatter_mm_placeholders)
-from vllm.logging_utils.dump_input import dump_engine_exception
 
 if TYPE_CHECKING:
     import xgrammar as xgr
@@ -1464,8 +1463,6 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         scheduler_output: "SchedulerOutput",
         intermediate_tensors: Optional[IntermediateTensors] = None,
     ) -> Union[ModelRunnerOutput, IntermediateTensors]:
-        dump_engine_exception(self.vllm_config, scheduler_output,
-                                  self.scheduler.make_stats())
         self._update_states(scheduler_output)
         if not scheduler_output.total_num_scheduled_tokens:
             if not has_kv_transfer_group():
