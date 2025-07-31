1315966594139face06b9ce8f4ff2fb4453228a0
wwl2755-google <wenlongwang@google.com>
2025-07-31 16:59:53 -0700

    [DEBUG] first half of rotary_pos_emb_thw()

--- vllm/model_executor/models/qwen2_5_vl.py
+++ vllm/model_executor/models/qwen2_5_vl.py
@@ -574,20 +574,22 @@ class Qwen2_5_VisionTransformer(nn.Module):
     def rotary_pos_emb_thw(self, t, h, w):
         hpos_ids = torch.arange(h).unsqueeze(1).expand(-1, w)
         wpos_ids = torch.arange(w).unsqueeze(0).expand(h, -1)
-        logger.info(f"[DEBUG] hpos_ids: {hpos_ids}, shape: {hpos_ids.shape}, dtype: {hpos_ids.dtype}")
-        logger.info(f"[DEBUG] wpos_ids: {wpos_ids}, shape: {wpos_ids.shape}, dtype: {wpos_ids.dtype}")
+        # logger.info(f"[DEBUG] hpos_ids: {hpos_ids}, shape: {hpos_ids.shape}, dtype: {hpos_ids.dtype}")
+        # logger.info(f"[DEBUG] wpos_ids: {wpos_ids}, shape: {wpos_ids.shape}, dtype: {wpos_ids.dtype}")
         hpos_ids = hpos_ids.reshape(
             h // self.spatial_merge_size,
             self.spatial_merge_size,
             w // self.spatial_merge_size,
             self.spatial_merge_size,
         ).permute(0, 2, 1, 3).flatten()
+        logger.info(f"[DEBUG] hpos_ids: {hpos_ids}, shape: {hpos_ids.shape}, dtype: {hpos_ids.dtype}")
         wpos_ids = wpos_ids.reshape(
             h // self.spatial_merge_size,
             self.spatial_merge_size,
             w // self.spatial_merge_size,
             self.spatial_merge_size,
         ).permute(0, 2, 1, 3).flatten()
+        logger.info(f"[DEBUG] wpos_ids: {wpos_ids}, shape: {wpos_ids.shape}, dtype: {wpos_ids.dtype}")
         pos_ids = torch.stack([hpos_ids, wpos_ids], dim=-1).repeat(t, 1)
         logger.info(f"[DEBUG] first half of rotary_pos_emb_thw(): pos_ids: {pos_ids}, shape: {pos_ids.shape}, dtype: {pos_ids.dtype}")
         max_size = max(h, w)
