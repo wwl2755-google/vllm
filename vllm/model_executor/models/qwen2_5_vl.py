0d590603ca54921eea8b3bd3c816dc5bb82c827f
wwl2755-google <wenlongwang@google.com>
2025-08-03 00:33:01 -0700

    make it work

--- vllm/model_executor/models/qwen2_5_vl.py
+++ vllm/model_executor/models/qwen2_5_vl.py
@@ -1106,18 +1106,18 @@ class Qwen2_5_VLForConditionalGeneration(nn.Module, SupportsMultiModal,
         multimodal_embeddings: Optional[MultiModalEmbeddings] = None,
     ) -> torch.Tensor:
         
-        logger.info(f"[DEBUG] mm embeddings: {multimodal_embeddings[0]}, shape: {multimodal_embeddings[0].shape}, dtype: {multimodal_embeddings[0].dtype}")
+        # logger.info(f"[DEBUG] mm embeddings: {multimodal_embeddings[0]}, shape: {multimodal_embeddings[0].shape}, dtype: {multimodal_embeddings[0].dtype}")
 
         inputs_embeds = self.language_model.get_input_embeddings(input_ids)
 
-        logger.info(f"[DEBUG] text embeddings: {inputs_embeds}, shape: {inputs_embeds.shape}, dtype: {inputs_embeds.dtype}")
+        # logger.info(f"[DEBUG] text embeddings: {inputs_embeds}, shape: {inputs_embeds.shape}, dtype: {inputs_embeds.dtype}")
 
         if multimodal_embeddings is not None \
             and len(multimodal_embeddings) != 0:
             inputs_embeds = merge_multimodal_embeddings(
                 input_ids, inputs_embeds, multimodal_embeddings,
                 [self.config.image_token_id, self.config.video_token_id])
-        logger.info(f"[DEBUG] inputs_embeds: {inputs_embeds}, shape: {inputs_embeds.shape}, dtype: {inputs_embeds.dtype}")
+        # logger.info(f"[DEBUG] inputs_embeds: {inputs_embeds}, shape: {inputs_embeds.shape}, dtype: {inputs_embeds.dtype}")
         return inputs_embeds
 
     def get_input_embeddings_v0(
